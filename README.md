6a6ed9ea66d0285c53b61a7d27f55849123ee855
meritxellbatalla <mbatallamercade@gmail.com>
2025-05-26 12:41:00 +0200

    Repository of the ionic balance funtion in R

---
+++ README.md
@@ -0,0 +1,82 @@
+# Ionic Balance and Alkalinity Quality Assessment Function
+
+## Description 
+
+This repository contains a function created for water chemical analysis quality assessment, based on ionic balance to detect potentially inaccurate measurements.
+
+The function assigns two quality flags according to a ionic balance threshold determined by a user-defined parameter ('high-quality' and 'low-quality') 
+and a third flag ('no ionic balance data'), when required information is missing.
+
+## Arguments
+
+pH = pH  
+ALK = Alkalinity in µeq/l  
+Ca = Calcium in mg/l or µeq/l  
+Cl = Chloride in mg/l or µeq/l  
+K = Potasium in mg/l or µeq/l  
+Mg = Magnessium in mg/l or µeq/l  
+Na = Sodium in mg/l or µeq/l  
+NH4 = Ammonium in mg/l or µeq/l  
+NO3 = Nitrate in mg/l or µeq/l  
+NO2 = Nitrite in mg/l or µeq/l  
+SO4 = Sulfate in mg/l or µeq/l  
+Cond = measured conductivity in µS/cm (at 20ºC)  
+IB_thres = Ionic balance threshold. Samples with a ionic balance less than or equal to this threshold (in absolute value) will be classified as 'high-quality',  
+> otherwise, they will be classified as 'low-quality'
+ALK.Dif_thres (optional) = Percentual difference between estimated and measured alkalinity threshold. If provided, samples with a difference (ALK.Dif) greater than or equal to 
+> this threshold (in absolute value) will be reclassified from 'high-quality' to 'low-quality'.If not specified, this check is skipped.
+> units_ueq = TRUE (default, µeq/l) or FALSE (mg/l)    
+
+## Units
+
+Major anions (Cl, SO4, NO3), major cations (Na, K, Ca, Mg) ammonium and nitrites can be expressed either in µeq/L or in mg/L, but all must be in the same unit.
+"If 'units_ueq = TRUE' (default), all concentrations must be in µeq/L. If FALSE, they must be in mg/L."
+Alkalinity and conductivity must be in µeq/l and µS/cm, respectively
+
+## Result
+
+The function returns a dataframe containing:
+* Quality flag (quality)
+* Ionic balance (I.Balance)
+* Percentual difference between estimated and measured alkalinity (ALK.Dif)
+* Percentual difference between ion-estimated and measured conductivities (Cond.Dif)
+  
+## Example
+
+```sh
+
+table<-data.frame(pH = c(7.2, 6.8), 
+                  ALK = c(80, 63), 
+                  Ca = c(50, 45), 
+                  Cl = c(8, 15), 
+                  K = c(2.1, 2.5), 
+                  Mg = c(10, 18), 
+                  Na = c(15, 24), 
+                  NH4 = c(0.5, 0.4), 
+                  NO3 = c(1.2, 1.1), 
+                  NO2 = c(0.01, 0.02), 
+                  SO4 = c(12, 8), 
+                  Cond = c(22, 55)) 
+
+result_qc <- ionic_balance_control(
+  pH = table$pH,
+  ALK = table$ALK,
+  Ca = table$Ca,
+  Cl = table$Cl,
+  K = table$K,
+  Mg = table$Mg,
+  Na = table$Na,
+  NH4 = table$NH4,
+  NO3 = table$NO3,
+  NO2 = table$NO2,
+  SO4 = table$SO4,
+  Cond = table$Cond,
+  IB_thres = 5,
+  ALK.Dif_thres = 100,
+  units_ueq = TRUE)
+  
+table <- bind_cols(table, result_qc)
+
+ ```
+  
+  
---
+++ ionic_balance_control_function.R
@@ -0,0 +1,107 @@
+######################### IONIC BALANCE AND ALKALINITY QUALITY ASSESSMENT FUNCTION ############################
+###############################################################################################################
+# Function created for water chemical analysis quality assessment, based on ionic balance to detect potentially 
+# inaccurate measurements. The function assigns two quality flags according to a ionic balance threshold 
+# determined by a user-defined parameter ('high-quality' and 'low-quality') and a third flag ('no ionic balance 
+# data'), when required information is missing.
+
+# List of function parameters:
+
+# pH = pH
+# ALK = Alkalinity in µeq/l
+# Ca = Calcium in mg/l or µeq/l
+# Cl = Chloride in mg/l or µeq/l
+# K = Potasium in mg/l or µeq/l
+# Mg = Magnessium in mg/l or µeq/l
+# Na = Sodium in mg/l or µeq/l
+# NH4 = Ammonium in mg/l or µeq/l
+# NO3 = Nitrate in mg/l or µeq/l
+# NO2 = Nitrite in mg/l or µeq/l
+# SO4 =Sulfate in mg/l or µeq/l
+# Cond = measured conductivity in µS/cm (at 20ºC)
+# IB_thres = Ionic balance threshold. Samples with a ionic balance less than or equal to this threshold (in absolute value) 
+      # will be classified as 'high-quality', otherwise, they will be classified as 'low-quality'
+# ALK.Dif_thres (optional) = Percentual difference between estimated and measured alkalinity threshold.
+      # A threshold value to assess discrepancies between estimated and measured alkalinity. 
+      # If provided, samples with a difference (ALK.Dif) greater than or equal to this threshold (in absolute value) 
+      # will be reclassified from 'high-quality' to 'low-quality'.If not specified, this check is skipped.
+# units_ueq = TRUE (default) or FALSE  
+      # Major anions (Cl, SO4, NO3), major cations (Na, K, Ca, Mg) ammonium and nitrites can be 
+      # expressed either in µeq/L or in mg/L, but all must be in the same unit.
+      # "If 'units_ueq = TRUE' (default), all concentrations must be in µeq/L. If FALSE, they must be in mg/L."
+      # Alkalinity and conductivity must be in µeq/l and µS/cm respectively
+
+ionic_balance_control<-function(pH, ALK, Ca, Cl, K, Mg, Na, NH4, NO3, NO2, SO4, Cond, IB_thres, ALK.Dif_thres = NULL, units_ueq = TRUE){
+  
+  if (!units_ueq) {
+  # Let's convert all the units to equivalents
+  H = 10^6 * 10^-(pH)
+  Ca = Ca * (1 / 40.079) * 2 * 1000
+  Cl = Cl * (1 / 35.453) * 1000
+  K = K * (1 / 39.0983) * 1000
+  Mg = Mg * (1 / 24.305) * 2 * 1000
+  Na = Na * (1 / 22.989769) * 1000
+  NH4 = NH4 * (1 / 18) * 1000
+  NO3 = NO3 * (1 / 62.0049) * 1000
+  NO2 = NO2 * (1 / 46.0055) * 1000
+  SO4 = SO4 * (1 / 96.06) * 2 * 1000
+  } else {
+  H=10^6 * 10^-(pH)  
+  }
+  
+  # In order not to lose sites that do not have NO2 but do have NO3, we will do the following. 
+  NO3NO2=rowSums(cbind(NO3, NO2), na.rm = TRUE)
+  NO3NO2=if_else(is.na(NO2) & is.na(NO3),NA, NO3NO2)
+  NO3NO2=if_else(is.na(NO3),NA, NO3NO2)
+  
+  # Now we search for the summatory of cations and anions and the corresponding ionic balance
+  C.sum=H+Ca+Mg+K+Na+NH4
+  A.sum=ALK+Cl+NO3NO2+SO4
+  I.Balance=100*(C.sum-A.sum)/(C.sum+A.sum)
+  
+  # In order not to lose sites that do not have NO2 but do have NO3, we will do the following.
+  NO3_cond=NO3*63.6
+  NO2_cond=NO2*71.8
+  NO3NO2_cond=rowSums(cbind(NO3_cond, NO2_cond), na.rm = TRUE)
+  NO3NO2_cond=if_else(is.na(NO2) & is.na(NO3),NA, NO3NO2_cond)
+  NO3NO2_cond=if_else(is.na(NO3),NA, NO3NO2_cond)
+  
+  # Expected conductivity calculation (Ref. Jordi!!!!!)
+  cond.est=(H*315.1+NH4*67 + Ca*54.3 + Mg*48.6 + Na*45.9 + K*67 + ALK*39.4 + SO4*71.2 + NO3NO2_cond + Cl*68)/1000
+  
+  # Ionic strength
+  ionic.strength=((H+NH4 + Ca*2 + Mg*2 + Na + K + ALK + SO4*2 + NO3NO2 + Cl)/1000/2000)
+  
+  # Ionic strength coefficient
+  ionic.strength.coef=10^(-0.5*(ionic.strength^(1/2*(1+ionic.strength^1/2))-0.3*ionic.strength))
+  
+  # Estimated conductivity corrected
+  cond.est.corr=(cond.est*(ionic.strength.coef^2))
+  
+  # Percentual difference between ion-estimated and measured conductivities 
+  Cond.Dif=((Cond - cond.est.corr)/cond.est.corr)*100
+  
+  # Estimated alkalinity
+  ALK.est=(Ca+Mg+K+Na+NH4-Cl-SO4-NO3NO2)
+  
+  # Percentual difference between estimated and measured alkalinity 
+  ALK.Dif=(ALK-ALK.est)/ALK.est*100
+  
+ # Quality flag
+  quality <- ifelse(is.na(I.Balance), "no ionic balance data",
+                    ifelse(I.Balance >= -1 * IB_thres & I.Balance <= IB_thres, "high-quality", "low-quality"))
+  if (!is.null(ALK.Dif_thres)) {
+    quality <- ifelse(quality == "high-quality" & abs(ALK.Dif) >= ALK.Dif_thres, "low-quality", quality)
+  }
+  # Result as tibble/data.frame
+  return(data.frame(
+    quality = quality,
+    I.Balance = I.Balance,
+    ALK.Dif = ALK.Dif,
+    Cond.Dif = Cond.Dif
+  ))
+  
+  }
+
+# way to join the result at the original table:
+# table <- bind_cols(table, result_qc)
